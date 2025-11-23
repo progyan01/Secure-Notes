a64429b62eff5f94360d33739e0b8b23f14250bd
Pragyan Ojha <pragyan0888@gmail.com>
2025-11-24 00:39:49 +0530

    Fixed a typo in function name

--- crypto.h
+++ crypto.h
@@ -4,6 +4,6 @@
 #define SYSTEM_KEY "GHOST_OF_TSUSHIMA"
 
 void vigenere_encrypt(const char* plaintext, char* key, char* ciphertext);
-void vignere_decrypt(const char* ciphertext, char* key, char* plaintext);
+void vigenere_decrypt(const char* ciphertext, char* key, char* plaintext);
 
 #endif
