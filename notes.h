91df8380c6c893a036b6065e80d0a7fa275a831e
Pragyan Ojha <pragyan0888@gmail.com>
2025-11-24 01:14:54 +0530

    Implemented prototype for searchNotes() function and added a timestamp field in Notes struct.

--- notes.h
+++ notes.h
@@ -1,6 +1,7 @@
 #ifndef NOTES_H_
 #define NOTES_H_
 #include "auth.h"
+#include <time.h>
 
 #define MAX_NOTE_CONTENT 1000 
 #define MAX_NOTES 500
@@ -10,6 +11,7 @@ typedef struct{
     char *content;
     char title[MAX_TITLE_LEN];
     char owner[MAX_USERNAME_LEN];
+    time_t created_at;
 } Note;
 
 void createNote(Note* all_notes, int* note_count, User* currentUser);
@@ -23,5 +25,7 @@ void modifyNote(Note* all_notes, int note_index, int note_count, User* currentUs
 void freeNotes(Note* all_notes, int note_count);
 
 void deleteNote(Note* all_notes, int note_index, int* note_count, User* currentUser);
+
+void searchNotes(Note* all_notes, int note_count, User* currentUser, char* query);
 
 #endif
