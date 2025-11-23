d1f6e4948974bef2a4f4ba52d3f98099e18675f9
Pragyan Ojha <pragyan0888@gmail.com>
2025-11-24 00:32:28 +0530

    Added a system key for password.

--- crypto.h
+++ crypto.h
@@ -1,6 +1,8 @@
 #ifndef CRYPTO_H
 #define CRYPTO_H
 
+#define SYSTEM_KEY "GHOST_OF_TSUSHIMA"
+
 void vigenere_encrypt(const char* plaintext, char* key, char* ciphertext);
 void vignere_decrypt(const char* ciphertext, char* key, char* plaintext);
 
