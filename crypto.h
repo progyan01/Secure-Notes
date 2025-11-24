ccbde34fcad7aeaf552efab2a827cfbe6ab4cd69
Pragyan Ojha <pragyan0888@gmail.com>
2025-11-24 15:43:28 +0530

    Fixed a minor bug

--- crypto.h
+++ crypto.h
@@ -3,7 +3,7 @@
 
 #define SYSTEM_KEY "GHOST_OF_TSUSHIMA"
 
-void vigenere_encrypt(const char* plaintext, char* key, char* ciphertext);
-void vigenere_decrypt(const char* ciphertext, char* key, char* plaintext);
+void vigenere_encrypt(const char* plaintext, const char* key, char* ciphertext);
+void vigenere_decrypt(const char* ciphertext, const char* key, char* plaintext);
 
 #endif
