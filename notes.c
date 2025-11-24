015587b3c945f0a68078f1675930239b0491da9e
Pragyan Ojha <95168860+progyan01@users.noreply.github.com>
2025-11-24 22:04:20 +0530

    Add comments

--- notes.c
+++ notes.c
@@ -26,15 +26,20 @@ void createNote(Note* all_notes, int* note_count, User* currentUser){
 
     printf("Enter your note: \n");
     char tempBuffer[MAX_NOTE_CONTENT]; 
+    //using a buffer to temporarily store the note and then copying it into the dynamically created content field of note struct
     if (fgets(tempBuffer, MAX_NOTE_CONTENT, stdin) != NULL){
         tempBuffer[strcspn(tempBuffer, "\n")] = '\0';
+        //replace the \n at the end that's present after taking input using fgets and replace it with \0 to denote end of the string
         
         new_note->content = (char*)malloc(strlen(tempBuffer) + 1);
         
         if(new_note->content != NULL){
             vigenere_encrypt(tempBuffer, currentUser->session_key, new_note->content);
+            //encrpyt it first
 
             new_note->created_at = time(NULL);
+            //save the time at which it was created
+            
             (*note_count)++;
             printf("\n");
             printf("Note created successfully!");
@@ -77,17 +82,21 @@ void readNote(Note* all_notes, int note_index, int note_count, User* currentUser
     printf("\n");
 
     char *plainText=(char*)malloc(strlen(all_notes[note_index].content) + 1);
+    //store the decrypted note into a dynamically allocated character array
 
     if(plainText){
         vigenere_decrypt(all_notes[note_index].content, currentUser->session_key, plainText);
+        //decrypt the note first
         
         printf("----- %s -----\n", all_notes[note_index].title);
         printf("%s\n", plainText);
         char *time_str = ctime(&all_notes[note_index].created_at);
+        //retrieve the time at which the note was created
         time_str[strcspn(time_str, "\n")] = 0; 
         printf("Created: %s\n", time_str);
         printf("-----------------------\n");
-        
+
+        //remove the note content from the memory immediately to prevent privacy breaches by memory scraping
         memset(plainText, 0, strlen(plainText));
         free(plainText);
     }
@@ -104,17 +113,9 @@ void modifyNote(Note* all_notes, int note_index, int note_count, User* currentUs
     }
     printf("\n");
 
-    printf("Current Title: %s\n", all_notes[note_index].title);
-    char *plainText = (char*)malloc(strlen(all_notes[note_index].content) + 1);
-    if(plainText){
-        vigenere_decrypt(all_notes[note_index].content, currentUser->session_key, plainText);
-        printf("Current Content: %s\n", plainText);
-
-        memset(plainText, 0, strlen(plainText));
-        free(plainText);
-    }
-    printf("\n");
-
+    //using the function to print the existing note
+    readNote(all_notes, note_index, note_count, currentUser);
+    
     printf("Enter new content: ");
     char tempBuffer[MAX_NOTE_CONTENT];
 
@@ -124,6 +125,7 @@ void modifyNote(Note* all_notes, int note_index, int note_count, User* currentUs
 
         all_notes[note_index].content = (char*)malloc(strlen(tempBuffer) + 1);
         vigenere_encrypt(tempBuffer, currentUser->session_key, all_notes[note_index].content);
+        //encrypt the newly modified note
         
         printf("Note updated successfully!\n");
     }
@@ -138,8 +140,10 @@ void deleteNote(Note* all_notes, int note_index, int* note_count, User* currentU
         printf("Error: You do not own this note.\n");
         return;
     }
+    //confirm that the current user actually owns the note
     free(all_notes[note_index].content);
 
+    //correspondigly shift every note by 1 to the left
     for (int i = note_index; i < *note_count - 1; i++) {
         all_notes[i] = all_notes[i + 1];
     }
@@ -159,6 +163,7 @@ void searchNotes(Note* all_notes, int note_count, User* currentUser, char* query
         if(plainText){
             vigenere_decrypt(all_notes[i].content, currentUser->session_key, plainText);
 
+            //searching in the title as well as the note contents
             if (strstr(all_notes[i].title, query) != NULL || 
                 strstr(plainText, query) != NULL) {
                 
@@ -179,6 +184,7 @@ void searchNotes(Note* all_notes, int note_count, User* currentUser, char* query
 }
 
 void freeNotes(Note* all_notes, int note_count){
+    //we need to free all the notes from the RAM before closing the program to prevent memory leaks
     for(int i = 0; i < note_count; i++){
         if(all_notes[i].content != NULL){
             free(all_notes[i].content);
