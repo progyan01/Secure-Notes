9bd7108c78a9543bab6e52003597ef5ca9fa9ee1
Guthikonda Yaswanth Nadh <yaswanthnadhguthikonda@gmail.com>
2025-11-24 15:37:06 +0530

    implemented  hashing functiom for password

--- crypto.h
+++ crypto.h
@@ -1,9 +1,9 @@
 #ifndef CRYPTO_H
 #define CRYPTO_H
 
-#define SYSTEM_KEY "GHOST_OF_TSUSHIMA"
 
 void vigenere_encrypt(const char* plaintext, char* key, char* ciphertext);
 void vigenere_decrypt(const char* ciphertext, char* key, char* plaintext);
+void hash_password(const char* password, char* outputBuffer)
 
 #endif
